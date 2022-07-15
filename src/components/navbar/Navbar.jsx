340806f015a03a244ee0b69f0b3ae2e9a084ef46
mkaruc <fatihmehmetkaruc@gmail.com>
2022-07-15 17:10:26 +0300

    registerisdone

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Main from '../../pages/main/Main'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbarContainer'>
+      <div className="homeLink">
+      <Main> <h1>React Movie App</h1> </Main>
+      </div>
+      <div className="buttons">
+        
+      </div>
+    </div>
   )
 }
 
