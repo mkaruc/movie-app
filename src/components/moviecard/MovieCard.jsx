4a31bebef071014df60493f080b1dba4869526f0
mkaruc <fatihmehmetkaruc@gmail.com>
2022-07-18 13:20:58 +0300

    searchisdone

--- src/components/moviecard/MovieCard.jsx
+++ src/components/moviecard/MovieCard.jsx
@@ -1,14 +1,17 @@
 import {useNavigate} from 'react-router-dom'
 
-const MovieCard = ({movieList,url}) => {
+const MovieCard = ({movieList, setId}) => {
   const navigate=useNavigate();
+  
   return (
     <div onClick={() =>
       navigate('moviedetail')
     }>
       {movieList?.map((item, index)=>(
+        
         <div key={index} className='container' >
-          <img src="https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}/{item.backdrop_path}" alt="" />
+          
+          <img src={`https://image.tmdb.org/t/p/w440_and_h660_face${item.poster_path}`} alt={item.original_title} />
           <h2>{item.original_title}</h2>
           <p>{item.overview}</p>
         </div>
